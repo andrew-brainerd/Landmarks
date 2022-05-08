370b47a162487ebe7789a078c78935e45b566160
Andrew Brainerd <drwb333@gmail.com>
2022-05-07 23:59:30 -0400

    Add initial text views

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.green)
+            .textCase(Text.Case.uppercase)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
+
