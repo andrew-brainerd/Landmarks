a80e1b49f755c5be4e5f7d37d13eeacb9eb2af71
Andrew Brainerd <drwb333@gmail.com>
2022-06-21 21:16:42 -0400

    Improve share performance

--- Landmarks/Views/PropertyDetail.swift
+++ Landmarks/Views/PropertyDetail.swift
@@ -32,7 +32,18 @@ struct PropertyDetail: View {
                     Button(action: {
                         guard let urlShare = URL(string: "https://not-toast-ui.herokuapp.com/properties/\(property.id)") else { return }
                         let activityVC = UIActivityViewController(activityItems: [urlShare], applicationActivities: nil)
-                        UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+                        guard let firstScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+                            return
+                        }
+
+                        guard let firstWindow = firstScene.windows.first else {
+                            return
+                        }
+
+                        guard let viewController = firstWindow.rootViewController else {
+                            return
+                        }
+                        viewController.present(activityVC, animated: true, completion: nil)
 
                     }) {
                         Image(systemName: "square.and.arrow.up")
